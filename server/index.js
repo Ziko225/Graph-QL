ff67ee4746f27ee48dfb12d8fa74514d04943840
Nazar <nazar@bychyk.it>
2023-08-09 17:46:32 +0200

    Added graphQL schema and interaction to the server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,37 @@
 const express = require("express");
-
+const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
+const schema = require("./schma.js");
+const users = [{ id: 1123, username: "Vasia", age: 23 }];
 
 const app = express();
 
 app.use(cors());
+
+const root = {
+    getAllUsers: () => {
+        return users;
+    },
+
+    getUser: ({ id }) => {
+        return users.find(user => user.id == id);
+    },
+
+    createUser: ({ user }) => {
+        const newUser = {
+            id: Date.now(),
+            ...user
+        };
+
+        users.push(newUser);
+        return newUser;
+    }
+};
+
+app.use("/graphql", graphqlHTTP({
+    graphiql: true,
+    schema,
+    rootValue: root,
+}));
 
 app.listen(5000, () => console.log("server starts"));
---
+++ server/schma.js
@@ -0,0 +1,35 @@
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+    type User {
+        id: ID
+        username: String
+        age: Int
+        posts: [Post]
+    }
+    type Post {
+        id: ID
+        title: String
+        content: String
+    }
+    
+    input UserInput {
+        username: String!
+        age: Int!
+        posts: [PostInput]
+    }
+    input PostInput {
+        title: String!
+        content: String!
+    }
+
+    type Query {
+        getAllUsers: [User]
+        getUser(id: ID): User
+    }
+    type Mutation {
+        createUser(user: UserInput): User
+    }
+`);
+
+module.exports = schema;
